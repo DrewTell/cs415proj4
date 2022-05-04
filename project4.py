c2cdb0713fb04c4456c8717384a72a3604e71c95
DrewTell <98501727+DrewTell@users.noreply.github.com>
2022-05-03 21:19:37 -0700

    Place holders for other tasks

--- project4.py
+++ project4.py
@@ -1,27 +1,63 @@
 def main():
 
     filesToOpen = fileToRead()
+
     print("File containing the capacity, weights, and values are:", filesToOpen[0], filesToOpen[1], filesToOpen[2], "\n")
     
     #filesToOpen[0] is the _c.txt file containing capacity
-    sackCapacity = capacity(filesToOpen[0])
-
     #filesToOpen[1] is the _w.txt file and every weight will be added to the list
     #filesToOpen[2] is the _v.txt file and every value will be added to the list
+    sackCapacity = capacity(filesToOpen[0])
     weightList = fileParser(filesToOpen[1])
     valueList = fileParser(filesToOpen[2])
     listSize = len(weightList)
+
     print("Knapsack capacity =", sackCapacity, "Total number of items =", listSize, "\n")
-    ansTask1A = task1A(sackCapacity, weightList, valueList, listSize)
-    print("File containing the capacy, weights, and values are:")
+
+    task1A(sackCapacity, weightList, valueList, listSize)
+    task1B(sackCapacity, weightList, valueList, listSize)
+    task1C(sackCapacity, weightList, valueList, listSize)
+    task2A(sackCapacity, weightList, valueList, listSize)
+    task2B(sackCapacity, weightList, valueList, listSize)
+
+def task1A(sackCapacity, weightList, valueList, listSize):
+    
+    ansTask1A = traditionalKnapSack(sackCapacity, weightList, valueList, listSize)
     print("(1a) Traditional Dynamic Programming Optimal value:", ansTask1A[0])
     print("(1a) Traditional Dynamic Programming Optimal subset:", ansTask1A[1])
-    print("(1a) Traditional Dynamic Programming Total Basic Ops:", ansTask1A[2])
+    print("(1a) Traditional Dynamic Programming Total Basic Ops:", ansTask1A[2], '\n')
 
+def task1B(sackCapacity, weightList, valueList, listSize):
+    
+    ansTask1B = memFuncKnapSack(sackCapacity, weightList, valueList, listSize)
+    print("(1b) Memory-function Dynamic Programming Optimal value:", ansTask1B[0])
+    print("(1b) Memory-function Dynamic Programming Optimal subset:", ansTask1B[1])
+    print("(1b) Memory-function Dynamic Programming Total Basic Ops:", ansTask1B[2], '\n')
 
+def task1C(sackCapacity, weightList, valueList, listSize):
+    
+    ansTask1C = spaceEfficientKnapSack(sackCapacity, weightList, valueList, listSize)
+    print("(1c) Space-efficient Dynamic Programming Optimal value:", ansTask1C[0])
+    print("(1c) Space-efficient Dynamic Programming Optimal subset:", ansTask1C[1])
+    print("(1c) Space-efficient Dynamic Programming Total Basic Ops:", ansTask1C[2], '\n')
+    print("(1c) Space-efficient Dynamic Programming Space Taken:", ansTask1C[3], '\n')
 
-#returns the file names to be used given an integer
+def task2A(sackCapacity, weightList, valueList, listSize):
+    
+    anstask2A = greedyApproach(sackCapacity, weightList, valueList, listSize)
+    print("(2a) Greedy Approach Optimal value:", anstask2A[0])
+    print("(2a) Greedy Approach Optimal subset:", anstask2A[1])
+    print("(2a) Greedy Approach Total Basic Ops:", anstask2A[2], '\n')
+
+def task2B(sackCapacity, weightList, valueList, listSize):
+    
+    ansTask2B = heapBased(sackCapacity, weightList, valueList, listSize)
+    print("(2b) Heap-based Greedy Approach Optimal value:", ansTask2B[0])
+    print("(2b) Heap-based Greedy Approach Optimal subset:", ansTask2B[1])
+    print("(2b) Heap-based Greedy Approach Total Basic Ops::", ansTask2B[2], '\n')
+
 def fileToRead():
+    #returns the file names to be used given an integer
     prefix = int(input("Enter a number to open associated files:"))
     nameGeneric = "p0"
     if (prefix > 9):
@@ -32,15 +68,17 @@ def fileToRead():
     fileValue = nameGeneric + "_v.txt"
     return fileCap, fileWeight, fileValue
 
-# capacity opens the _w.txt file to read the first value for the total weight that can be held
+
 def capacity(fileName):
+    # capacity opens the _w.txt file to read the first value for the total weight that can be held
     fileData = open(fileName, 'r')
     capacity = fileData.read()
     fileData.close()
     return int(capacity)
 
-# creates a list of values from a text file
+
 def fileParser(fileName):
+    # creates a list of values from a text file
     fileData = open(fileName, 'r')
     weightSet = []
     for x in fileData:
@@ -49,7 +87,8 @@ def fileParser(fileName):
     return weightSet
 
 
-def task1A(capacity, weight, value, size):
+
+def traditionalKnapSack(capacity, weight, value, size):
     #table creation for a 2 dimensional table where capacity + 1 will create x cells for each integer up to capacity
     #ex capacity = 3: span will be 0,1,2,3
     #range of size will dictate how many rows there are in the 2-dimensional array
@@ -113,6 +152,22 @@ def task1A(capacity, weight, value, size):
 
 
     return table[size][capacity], itemIndexes, numOfOps   # returning the maximum value of knapsack and the index of items in the table where the first item is item 1    
+
+def memFuncKnapSack(capacity, weight, value, size):
+    print("PlaceHolder 1B")
+    return 0, 0, 0
+
+def spaceEfficientKnapSack(capacity, weight, value, size):
+    print("PlaceHolder 1C")
+    return 0, 0, 0, 0
+
+def greedyApproach(capacity, weight, value, size):
+    print("PlaceHolder 2A")
+    return 0, 0, 0, 0
+
+def heapBased(capacity, weight, value, size):
+    print("PlaceHolder 2B")
+    return 0, 0, 0
 
 
 if __name__ == "__main__":
